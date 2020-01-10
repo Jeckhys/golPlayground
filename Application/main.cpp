292f40cfb7c865186f1b615f7416d11d911073f1
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-01-10 16:48:08 +0900

    Updated window resolution

--- Application/main.cpp
+++ Application/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char * argv[])
 {
-    GuiWindow golWindow({1600, 1280}, "golPlayground", 32.0f);
+    GuiWindow golWindow({1280, 800}, "golPlayground", 32.0f);
     golWindow.setFps(60);
 
     golWindow.run();
